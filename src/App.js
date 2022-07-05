69b8d30c89023b2330b172ec24d7d18424e4d77e
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-07-05 12:38:50 +0200

    useEffect Hooks add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import StateTutorial from './UseState/StateTutorial';
 import ReducerState from './UseReducer/ReducerState';
+import Effect from './useEffect/Effect';
 
 function App() {
   return (
     <div className="App">
       <StateTutorial />
+      <hr></hr>
       <ReducerState />
+      <hr></hr>
+      <Effect />
+      <hr></hr>
     </div>
   );
 }
