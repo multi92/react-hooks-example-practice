2940ba1e62ba4779d2796bcfc8b18690e437e127
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-07-05 11:25:27 +0200

    useReducesHookAdd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import StateTutorial from './UseState/StateTutorial';
+import ReducerState from './UseReducer/ReducerState';
 
 function App() {
   return (
     <div className="App">
       <StateTutorial />
+      <ReducerState />
     </div>
   );
 }
